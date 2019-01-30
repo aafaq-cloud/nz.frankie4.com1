f7dfede5952fd895825259f6bfb4018b5fc18fd6
Carlton H <carlton@lambagency.com.au>
2019-01-30 15:26:32 +1000

    product detail buttons, selectors

--- src/assets/scripts/class/Product.js
+++ src/assets/scripts/class/Product.js
@@ -419,14 +419,14 @@ export class Product extends EventEmitter {
       clearTimeout(clearErrorTimeout);
 
       this.selectorsSingle.addToCart.classList.add("js-loading", "no-hover");
-      this.selectorsSingle.addToCartText.innerHTML = "Adding to cart";
+      this.selectorsSingle.addToCartText.innerHTML = "Adding to bag";
 
       // Trigger an add to cart ajax function
       AppShopifyCart.addItem(variantID, quantity, properties).then(() => {
           // If we successfully added the item to cart we will show a success message
           this.selectorsSingle.addToCart.classList.remove("js-loading");
           this.selectorsSingle.addToCart.classList.add("added");
-          this.selectorsSingle.addToCartText.innerHTML = "Added to cart";
+          this.selectorsSingle.addToCartText.innerHTML = "Added to bag";
 
           // Clear the success button styles
           setTimeout(() => {
