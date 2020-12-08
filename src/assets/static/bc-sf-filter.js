6c3c3092497f81fbbc3e1003cd8ec9e72cf1b058
Howard Chen <>
2020-12-09 09:15:28 +1000

    swym wishlist colour changed and attempt of adding wishlist button to quickview and product collection tiles

--- src/assets/static/bc-sf-filter.js
+++ src/assets/static/bc-sf-filter.js
@@ -21,7 +21,9 @@ var bcSfFilterTemplate = {
     // Grid Template
     'productGridItemHtml': ' <div class="cell small-6 large-3 grid-x align-stretch {{itemHandle}} {{isPrimary}}">' +
         '<article class="product-tile text-center grid-y" data-handle="{{itemHandle}}">' +
-        '<div class="cell product-tile__image-container">' +
+        // '{% include "swym-product-view", product: product %}' +
+        // '<button class="swym-button swym-add-to-wishlist-view-product product_{{product.id}}" data-swaction="addToWishlist" data-product-id="{{product.id | json}}" style="font-size: 25px; width: 25px"></button>' +
+        '<div class="cell product-tile__image-container">' + 
         '<a href="{{itemUrl}}" class="cover-link" tabindex="-1" aria-hidden="true"></a>' +
         '{{itemImages}}' +
         '<button data-product-quickview data-product-url="{{itemUrl}}" class="product-tile__quickview button button-secondary button-small">Quickview</button>' +
