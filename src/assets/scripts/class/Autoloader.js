3567fe9e120428008c3c55f7acb0f6a2656051b8
Caitlin C <caitlin.c@lambagency.com.au>
2021-01-28 16:43:41 +1000

    Fix merge issue

--- src/assets/scripts/class/Autoloader.js
+++ src/assets/scripts/class/Autoloader.js
@@ -54,7 +54,7 @@ export class Autoloader {
       SneakerGuideTabsSection,
       BannerSection,
       ShippingSection,
-      StepInsideSection
+      StepInsideSection,
       WeStandForSection
     };
 
